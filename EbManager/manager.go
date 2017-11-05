26c94ea0ed78c88c722ec00a6ffd18eae680f341
deepaksinghmehta <deepaksinghmehta@gmail.com>
2017-11-05 22:29:26 +0000

    Update manager.go

--- EbManager/manager.go
+++ EbManager/manager.go
@@ -6,10 +6,10 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
-	"os"
-	"encoding/json"
-	"text/template"
-	"log"
+	//"os"
+	//"encoding/json"
+	//"text/template"
+	//"log"
 )
 
 var settings struct {
@@ -17,6 +17,102 @@ var settings struct {
 	SourceDir  string `json:"sourceDir"`
 	TargetDir  string `json:"targetDir"`
 	Local_token 	   string `json:"local_token"`
+}
+
+
+// To configure option settings
+//
+// The following operation configures several options in the aws:elb:loadbalancer namespace:
+func ExampleElasticBeanstalk_UpdateEnvironment_shared01() {
+	//svc := elasticbeanstalk.New(session.New())
+	svc := elasticbeanstalk.New(session.New(), aws.NewConfig().WithRegion("eu-west-1"))
+	input := &elasticbeanstalk.UpdateEnvironmentInput{
+		EnvironmentName: aws.String("docker-multicontainer-v2-dev"),
+		OptionSettings: []*elasticbeanstalk.ConfigurationOptionSetting{
+			{
+				Namespace:  aws.String("aws:autoscaling:scheduledaction:"),
+				ResourceName: aws.String("ScheduledPeriodicScaleUp"),
+				OptionName: aws.String("MinSize"),
+				Value:      aws.String("2"),
+			},
+			{
+				Namespace:  aws.String("aws:autoscaling:scheduledaction:"),
+				ResourceName: aws.String("ScheduledPeriodicScaleUp"),
+				OptionName: aws.String("MaxSize"),
+				Value:      aws.String("4"),
+			},
+			{
+				Namespace:  aws.String("aws:autoscaling:scheduledaction:"),
+				ResourceName: aws.String("ScheduledPeriodicScaleUp"),
+				OptionName: aws.String("DesiredCapacity"),
+				Value:      aws.String("2"),
+			},
+			{
+				Namespace:  aws.String("aws:autoscaling:scheduledaction:"),
+				ResourceName: aws.String("ScheduledPeriodicScaleUp"),
+				OptionName: aws.String("StartTime"),
+				Value:      aws.String("2018-01-14T07:00:00Z"),
+			},
+			{
+				Namespace:  aws.String("aws:autoscaling:scheduledaction:"),
+				ResourceName: aws.String("ScheduledPeriodicScaleUp"),
+				OptionName: aws.String("EndTime"),
+				Value:      aws.String("2018-05-14T07:00:00Z"),
+			},
+			{
+				Namespace:  aws.String("aws:autoscaling:scheduledaction:"),
+				ResourceName: aws.String("ScheduledPeriodicScaleUp"),
+				OptionName: aws.String("Recurrence"),
+				Value:      aws.String("0 18 * * 5"),
+			},
+		},
+	}
+
+	result, err := svc.UpdateEnvironment(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case elasticbeanstalk.ErrCodeInsufficientPrivilegesException:
+				fmt.Println(elasticbeanstalk.ErrCodeInsufficientPrivilegesException, aerr.Error())
+			case elasticbeanstalk.ErrCodeTooManyBucketsException:
+				fmt.Println(elasticbeanstalk.ErrCodeTooManyBucketsException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+}
+
+// To view a list of applications
+//
+// The following operation retrieves information about applications in the current region:
+func ExampleElasticBeanstalk_DescribeApplications_shared00() {
+	svc := elasticbeanstalk.New(session.New(), aws.NewConfig().WithRegion("eu-west-1"))
+	input := &elasticbeanstalk.DescribeApplicationsInput{}
+
+	result, err := svc.DescribeApplications(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+	fmt.Println("printing result")
+	fmt.Println(result)
 }
 
 //this method is used to create application
@@ -45,42 +141,98 @@ func creteebApplication(){
 		}
 		return
 	}
-
+	fmt.Println("printing result")
 	fmt.Println(result)
 }
 
+// To configure option settings
+//
+// The following operation configures several options in the aws:elb:loadbalancer namespace:
+//func ExampleElasticBeanstalk_UpdateEnvironment_shared01() {
+//	svc := elasticbeanstalk.New(session.New())
+//	input := &elasticbeanstalk.UpdateEnvironmentInput{
+//		EnvironmentName: aws.String("my-env"),
+//		OptionSettings: []*elasticbeanstalk.ConfigurationOptionSetting{
+//			{
+//				Namespace:  aws.String("aws:elb:healthcheck"),
+//				OptionName: aws.String("Interval"),
+//				Value:      aws.String("15"),
+//			},
+//			{
+//				Namespace:  aws.String("aws:elb:healthcheck"),
+//				OptionName: aws.String("Timeout"),
+//				Value:      aws.String("8"),
+//			},
+//			{
+//				Namespace:  aws.String("aws:elb:healthcheck"),
+//				OptionName: aws.String("HealthyThreshold"),
+//				Value:      aws.String("2"),
+//			},
+//			{
+//				Namespace:  aws.String("aws:elb:healthcheck"),
+//				OptionName: aws.String("UnhealthyThreshold"),
+//				Value:      aws.String("3"),
+//			},
+//		},
+//	}
+//
+//	result, err := svc.UpdateEnvironment(input)
+//	if err != nil {
+//		if aerr, ok := err.(awserr.Error); ok {
+//			switch aerr.Code() {
+//			case elasticbeanstalk.ErrCodeInsufficientPrivilegesException:
+//				fmt.Println(elasticbeanstalk.ErrCodeInsufficientPrivilegesException, aerr.Error())
+//			case elasticbeanstalk.ErrCodeTooManyBucketsException:
+//				fmt.Println(elasticbeanstalk.ErrCodeTooManyBucketsException, aerr.Error())
+//			default:
+//				fmt.Println(aerr.Error())
+//			}
+//		} else {
+//			// Print the error, cast err to awserr.Error to get the Code and
+//			// Message from an error.
+//			fmt.Println(err.Error())
+//		}
+//		return
+//	}
+//
+//	fmt.Println(result)
+//}
+
 func main() {
-	t, err := template.ParseFiles( "envconfig.yaml.template")
-	if err != nil {
-		log.Print(err)
-		return
-	}
-
-	f, err := os.Create("envconfig.yaml")
-	if err != nil {
-		log.Println("create file: ", err)
-		return
-	}
-
-	// then config file settings
-
-	configFile, err := os.Open("config.json")
-	if err != nil {
-		fmt.Println("opening config file", err.Error())
-	}
-	defer configFile.Close()
-	jsonParser := json.NewDecoder(configFile)
-	if err = jsonParser.Decode(&settings); err != nil {
-		fmt.Println("parsing config file", err.Error())
-	}
+	//ExampleElasticBeanstalk_DescribeApplications_shared00()
+	ExampleElasticBeanstalk_UpdateEnvironment_shared01()
+	//creteebApplication()
+	//t, err := template.ParseFiles( "envconfig.yaml.template")
+	//if err != nil {
+	//	log.Print(err)
+	//	return
+	//}
+	//
+	//f, err := os.Create("envconfig.yaml")
+	//if err != nil {
+	//	log.Println("create file: ", err)
+	//	return
+	//}
+	//
+	//// then config file settings
+	//
+	//configFile, err := os.Open("config.json")
+	//if err != nil {
+	//	fmt.Println("opening config file", err.Error())
+	//}
+	//defer configFile.Close()
+	//jsonParser := json.NewDecoder(configFile)
+	//if err = jsonParser.Decode(&settings); err != nil {
+	//	fmt.Println("parsing config file", err.Error())
+	//}
 
 	//fmt.Printf("%v %s %s %s", settings.ServerMode, settings.SourceDir, settings.TargetDir, settings.Locale)
 
-	err = t.Execute(f, settings)
-	if err != nil {
-		log.Print("execute: ", err)
-		return
-	}
+	//err = t.Execute(f, settings)
+	//if err != nil {
+	//	log.Print("execute: ", err)
+	//	return
+	//}
 	return
 
 }
